8418f1060e41987cc410275e2be40991bdcdce9a
Ankit Mishra <ankitmishra16127@gmail.com>
2025-10-28 22:14:26 +0530

    debug some issues

--- Backend/data/products.ts
+++ Backend/data/products.ts
@@ -7,49 +7,49 @@ export const products:Product[]=[
         id:1,
         name:"Wireless Mouse",
         price:799,
-        image:"https://via.placeholder.com/150"
+        image:"https://tse2.mm.bing.net/th/id/OIP.lER3YZebzgAqm9sp2AA1kwHaHa?rs=1&pid=ImgDetMain&o=7&rm=3"
     },
     {
         id:2,
         name:"Mechanical Keyboard",
         price:2999,
-        image:"https://via.placeholder.com/150"
+        image:"https://tse4.mm.bing.net/th/id/OIP.iJqi_sQZPWj82neUYbJ4wQHaEK?rs=1&pid=ImgDetMain&o=7&rm=3"
     },
     {
         id:3,
         name:"HD Monitor",
         price:299,
-        image:"https://via.placeholder.com/150"
+        image:"https://www.lg.com/in/images/monitors/md05750468/gallery/Global_MP68VQ_2016_Gallery_medium01.jpg"
     },
     {
         id:4,
         name:"USB-C-Hub",
         price:1999,
-        image:"https://via.placeholder.com/150"
+        image:"https://tse4.mm.bing.net/th/id/OIP.auQrTDfgfbS_OZcppYu5hwHaF4?rs=1&pid=ImgDetMain&o=7&rm=3"
     },
     {
         id:5,
         name:"External Hard Drive",
         price:4999,
-        image:"https://via.placeholder.com/150"
+        image:"https://m.media-amazon.com/images/I/71J5iVXpuZL.jpg"
     },
     {
         id:6,
         name:"Bluetooth Speaker",
         price:1499,
-        image:"https://via.placeholder.com/150"
+        image:"https://i5.walmartimages.com/asr/cdaaab64-5ea7-4d8f-969f-cff666244723.ff62693361b001ebbaf41145d23c0f13.jpeg"
     },
     {
         id:7,
         name:"Webcam",
         price:899,
-        image:"https://via.placeholder.com/150"
+        image:"https://i5.walmartimages.com/asr/912afc88-036b-404c-aa5f-a37c2bbc3315.871becce153588569f413746f945e50f.jpeg"
     },
     {
         id:8,
         name:"Headphones",
         price:1999,
-        image:"https://via.placeholder.com/150"
+        image:"https://m.media-amazon.com/images/I/811RreTN3rL.jpg"
     },
     
 ]
--- Frontend/vite-project/src/pages/Products.tsx
+++ Frontend/vite-project/src/pages/Products.tsx
@@ -2,10 +2,11 @@ import { useEffect, useState } from 'react';
 import type { Product } from '../types/product';
 import { getProducts } from '../services/productServices';
 import { useCart } from '../context/cartContext';
+import Promotional from '../component/Promotional';
 
 const Products = () => {
     const [products, setProducts] = useState<Product[]>([]);
-    const {addToCart} = useCart();
+    const { addToCart } = useCart();
 
     useEffect(() => {
         const fetchProducts = async () => {
@@ -16,17 +17,21 @@ const Products = () => {
     }, [])
 
     return (
-        <div style={{
-            display: "grid", gridTemplateColumns: "repeat(3,1fr)", gap: "20px", padding: "20px"
-        }}>
-            {products.map((p) => (
-                <div key={p.id} style={{ border: "1px solid gray", padding: "10px", borderRadius: "8px" }}>
-                    <img src={p.image} alt={p.name} style={{ width: "100%", height: "120px", objectFit: "cover" }} />
-                    <h3>{p.name}</h3>
-                    <p>₹{p.price}</p>
-                    <button onClick={() => addToCart(p.id)}>Add to Cart</button>
-                </div>
-            ))}
+        <div>
+            <Promotional />
+
+            <div style={{
+                display: "grid", gridTemplateColumns: "repeat(3,1fr)", gap: "20px", padding: "20px"
+            }}>
+                {products.map((p) => (
+                    <div key={p.id} style={{ border: "1px solid gray", padding: "10px", borderRadius: "8px" }}>
+                        <img src={p.image} alt={p.name} style={{ width: "100%", height: "120px", objectFit: "cover" }} />
+                        <h3>{p.name}</h3>
+                        <p>₹{p.price}</p>
+                        <button onClick={() => addToCart(p.id)}>Add to Cart</button>
+                    </div>
+                ))}
+            </div>
         </div>
     )
 }
